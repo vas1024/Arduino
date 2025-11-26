e06f486224f55f96cd953ea22f03ef55a745cd95
i5nuc <ip14922@gmail.com>
2025-11-26 19:24:14 +0300

    leds

--- mqtt_wifi/mqtt_wifi.ino
+++ mqtt_wifi/mqtt_wifi.ino
@@ -1,27 +1,39 @@
 #include <ESP8266WiFi.h> 
 #include <PubSubClient.h>
 //#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
-#include <FastLED.h>
+//#include <FastLED.h>
+#include <Adafruit_NeoPixel.h>
 
 //  i choose board = Generic ESP8266 Module
 
-#define NUM_LEDS 10
+//#define NUM_LEDS 180  // my tape has 180 leds
+#define NUM_LEDS 180
 //#define STRIP_PIN 2  // D2 = GPIO 4
-#define STRIP_PIN 4
+#define STRIP_PIN 2
 
-CRGB leds[NUM_LEDS];
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, STRIP_PIN, NEO_GRB + NEO_KHZ800);
 int arr[NUM_LEDS];
+bool newMessageInQ = false; 
+bool ledsOn = false;
 
-const char* ssid = "LENIVEC";
-const char* password = "z9c3-39yw-84r1";
-const char* mqtt_server = "192.168.1.100";
+const char* ssid = "Paris";
+const char* password = "ps97889600P";
+const char* mqtt_server = "192.168.1.101";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+
 void setup() {
 
   Serial.begin(115200);
+
+  // Инициализация ленты
+  strip.begin();
+  strip.setBrightness(50);  // Яркость от 0 до 255
+  strip.show();  // Очищаем ленту
+
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -45,38 +57,64 @@ void setup() {
     }
   }
 
-  FastLED.addLeds<WS2811, STRIP_PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(50);
- // pinMode(13, OUTPUT);
-
 }
 
+
+
 void loop() {
+
+  yield();
+
   if (!client.connected()) {
     reconnect();
   }
+
   client.loop();
+
+  if( newMessageInQ ){
+    newMessageInQ = false;
+/*    
+    if( ledsOn ) {
+      ledsOn = false;
+      for(int i = 0; i < NUM_LEDS; i++) {
+        strip.setPixelColor(i, 0);  
+      }
+      strip.show();
+    }
+    else {
+      ledsOn = true;
+      for( int i = 0; i < NUM_LEDS; i++ ){
+        arr[i] = i;
+      }
+      for (int i = 0; i < NUM_LEDS; i++)  strip.setPixelColor(i, strip.ColorHSV(arr[i] * 256, 123, 123));
+      strip.show();
+    }
+*/
+
+
+  for (int i = 0; i < NUM_LEDS; i++)  {
+    strip.setPixelColor(i, strip.ColorHSV(arr[i] * 256, 254, 64));
+    if (i % 20 == 0) {
+      yield();
+    }
+  }
+
+  strip.show();
+
+
+  }
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (unsigned int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
 
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  
+  newMessageInQ = true;
   parseCSV( (char*)payload, length );
 
-  for (int i = 0; i < NUM_LEDS; i++) {
-    Serial.println(arr[i]);
-  }
-  for (int i = 0; i < NUM_LEDS; i++) leds[i] = CHSV( arr[i], 255, 255 ) ;
-  FastLED.show();
-  for (int i = 0; i < NUM_LEDS; i++) arr[i] = 0;
-
 } 
+
+
 
 void reconnect() {
   while (!client.connected()) {
@@ -94,15 +132,10 @@ void reconnect() {
 }
 
 
-// Функция для конвертации CSV строки в массив целых чисел global int arr[NUM_LEDS]
-void parseCSV(const char* payload, unsigned int length ) {
-  // Преобразуем payload в строку
-  String data = String(payload);
-  Serial.println( "inside function parseCSV");
-  Serial.println( data );
-  Serial.print("length = ");
-  Serial.println(length);
 
+// Функция для конвертации CSV строки в массив целых чисел global int arr[NUM_LEDS]
+// проходит payload один раз и использует два указателя:  i - текущая позиция в payload, j - текущая позиция в arr
+void parseCSV(const char* payload, unsigned int length ) {
 
   int j = 0;
   int i = 0;
@@ -119,6 +152,9 @@ void parseCSV(const char* payload, unsigned int length ) {
       }
     }
     i++;
+    if(i % 50 == 0) {  
+      yield();         
+    }
   }
   if( num_str.length() > 0 and j < NUM_LEDS ){
     arr[j] = num_str.toInt();
